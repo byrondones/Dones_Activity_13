3982c6cb7ada3d03cb93aafb11db4fcba20670be
itstrazu <donesbyron05@gmail.com>
2023-10-20 08:04:36 +0800

    added header component that passes suitable props to header

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(footer){
+
+    return(
+    
+        <>
+            <footer className="fixed-bottom text-center m-3 fw-bold">{footer.footer}</footer>
+        </>
+    
+    )
+    
+    }
+    
+    export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Header(header,counter){
+
+return(
+    <>
+        <h1 className="text-center p-3">{header.title}</h1> 
+    </>
+)
+
+}
+
+export default Header
