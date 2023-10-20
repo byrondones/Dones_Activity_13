699e30ecdae5f80e34a93403601c940071bae29d
itstrazu <donesbyron05@gmail.com>
2023-10-20 08:05:53 +0800

    added components header counter and footer so it can render to site

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+import Header from './Header'
+import Footer from './Footer'
+import Counter from './Counter'
+
+function App() {
+
+  return (
+    <>
+      <Header title="Acivity 13" />
+      <Counter/>
+      <Footer footer="Copyright, 2023" />
+    </>
+  )
+}
+
+export default App
